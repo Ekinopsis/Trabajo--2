a81bcd42d1919cd103284cb3ddbc22261d979ee7
Ekinopsis <111795037+Ekinopsis@users.noreply.github.com>
2022-12-05 17:01:05 -0500

    Add files via upload

---
+++ trabajo 2/registro.php
@@ -0,0 +1,86 @@
+
+<?php
+//validamos datos del servidor
+$user = "root";
+$pass = "";
+$host = "localhost";
+
+//conetamos al base datos
+$connection = mysqli_connect($host, $user, $pass);
+
+//hacemos llamado al imput de formuario
+$PrimerNombre = $_POST["Primer Nombre"] ;
+$SegundoNombre = $_POST["Segundo nombre"] ;
+$PrimerApellido = $_POST["Primer apellido"] ;
+$SegundoApellido = $_POST["Segundo apellido"] ;
+$Ciudad = $_POST["Ciudad"] ;
+$Telefono = $_POST["Telefono"] ;
+
+//verificamos la conexion a base datos
+if(!$connection) 
+        {
+            echo "No se ha podido conectar con el servidor" . mysql_error();
+        }
+  else
+        {
+            echo "<b><h3>Hemos conectado al servidor</h3></b>" ;
+        }
+        //indicamos el nombre de la base datos
+        $datab = "trabajo2";
+        //indicamos selecionar ala base datos
+        $db = mysqli_select_db($connection,$datab);
+
+        if (!$db)
+        {
+        echo "No se ha podido encontrar la Tabla";
+        }
+        else
+        {
+        echo "<h3>Tabla seleccionada:</h3>" ;
+        }
+        //insertamos datos de registro al mysql xamp, indicando nombre de la tabla y sus atributos
+        $instruccion_SQL = "INSERT INTO formulario (Primer Nombre, Segundo nombre, Primer apellido, Segundo apellido, Ciudad, Telefono)
+                             VALUES ('$PrimerNombre','$SegundoNombre','$PrimerApellido','$SegundoApellido','$Ciudad','$Telefono')";
+                           
+                            
+        $resultado = mysqli_query($connection,$instruccion_SQL);
+
+        //$consulta = "SELECT * FROM tabla where id ='2'"; si queremos que nos muestre solo un registro en especifivo de ID
+        $consulta = SELECT * FROM "formulario";
+        
+$result = mysqli_query($connection,$consulta);
+if(!$result) 
+{
+    echo "No se ha podido realizar la consulta";
+}
+echo "<table>";
+echo "<tr>";
+echo "<th><h1>Primer Nombre</th></h1>";
+echo "<th><h1>Segundo nombre</th></h1>";
+echo "<th><h1>Primer apellido</th></h1>";
+echo "<th><h1>Segundo apellido</th></h1>";
+echo "<th><h1>Ciudad</th></h1>";
+echo "<th><h1>Telefono</th></h1>";
+echo "</tr>";
+
+while ($colum = mysqli_fetch_array($result))
+ {
+    echo "<tr>";
+    echo "<td><h2>" . $colum['Primer Nombre']. "</td></h2>";
+    echo "<td><h2>" . $colum['Segundo nombre']. "</td></h2>";
+    echo "<td><h2>" . $colum['Primer apellido'] . "</td></h2>";
+    echo "<td><h2>" . $colum['Segundo apellido'] . "</td></h2>";
+    echo "<td><h2>" . $colum['Ciudad'] . "</td></h2>";
+    echo "<td><h2>" . $colum['Telefono'] . "</td></h2>";
+    echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close( $connection );
+
+   //echo "Fuera " ;
+   echo'<a href="añadir.html"> Volver Atrás</a>';
+
+
+?>
+
